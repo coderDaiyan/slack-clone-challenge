16b4725e619ac91de39bd8dde3f1502aec4e475f
coderDaiyan <daiyanabdallah@gmail.com>
2021-03-12 21:39:09 +0600

    first commit and done the header section

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Chat from "./Components/Chat";
+import styled from "styled-components";
+import Header from "./Components/Header";
+import Sidebar from "./Components/Sidebar";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Container>
+          <Header />
+          <Main>
+            <Sidebar />
+            <Switch>
+              <Router path="/room">
+                <Chat />
+              </Router>
+              <Route exact path="/">
+                <Chat />
+              </Route>
+            </Switch>
+          </Main>
+        </Container>
+      </Router>
     </div>
   );
 }
 
 export default App;
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: grid;
+  grid-template-rows: 38px auto;
+`;
+
+const Main = styled.div`
+  display: grid;
+  grid-template-columns: 260px auto;
+`;
